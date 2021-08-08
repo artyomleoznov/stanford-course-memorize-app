947385e2f37d9a03d5a5495868b110b5ba9c66a0
leoznov <artem.leoznov@sbermegamarket.ru>
2021-08-08 15:20:12 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Артём Леознов on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
